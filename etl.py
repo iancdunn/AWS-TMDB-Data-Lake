f2ae28a982a7731d5709f6d8dd545a475e7302c2
Ian Dunn <padawian@gmail.com>
2026-01-12 13:46:38 -0600

    more user-friendly column names in latest update
    Renamed columns in the top 5 movies DataFrame for clarity.

--- etl.py
+++ etl.py
@@ -30,6 +30,7 @@ def extract_transform_load():
 
     #Generates a static view for quick consumption
     top_5 = df.head(5)[['rank', 'title', 'vote_average']]
+    top_5 = top_5.rename(columns = {'rank': 'Rank', 'title': 'Movie', 'vote_average': 'Rating'}
     
     with open('LATEST_UPDATE.md', 'w') as f:
         f.write(f"# Daily Movie Trends: {curr_date}\n\n")
@@ -40,3 +41,4 @@ if __name__ == "__main__":
         raise ValueError("Missing TMDB API Key.")
     
     extract_transform_load()
+
