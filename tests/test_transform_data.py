810eee566b5961fcbad3ed58056d29b34b30ad15
Ian Dunn <padawian@gmail.com>
2026-01-13 12:38:27 -0600

    Add files via upload

---
+++ tests/test_transform_data.py
@@ -0,0 +1,27 @@
+from etl import transform_data
+
+def test_transform_valid_data():
+    test_json = {'results':
+                    [{'title': "Test Movie",
+                     'popularity': 100.0,
+                     'vote_average': 5.0,
+                     'release_date': "1999-12-31"}]}
+    
+    res = transform_data(test_json, "2000-01-01")
+
+    assert len(res) == 1
+    assert res[0]['title'] == "Test Movie"
+    assert res[0]['popularity'] == 100.0
+    assert res[0]['vote_average'] == 5.0
+    assert res[0]['release_date'] == "1999-12-31"
+
+def test_transform_missing_data():
+    test_json = {'results':
+                    [{'title': "Test Movie",
+                     'popularity': 100.0,
+                     'vote_average': 5.0}]}
+    
+    res = transform_data(test_json, "2000-01-01")
+
+    assert res[0]['title'] == "Test Movie"
+    assert res[0]['release_date'] == "N/A"
